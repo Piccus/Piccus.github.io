bafb7ab98f04722982e9adff0b573d971d786676
Piccus Anda <piccus@outlook.com>
2016-07-16 17:54:39 +0800

    clean useless code

--- js/post.js
+++ js/post.js
@@ -1,21 +1,8 @@
 $(function() {
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'piccus'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-       var d = document, s = d.createElement('script');
-        
-        s.src = '//piccus.disqus.com/embed.js';
-        
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    });
     
     $.getScript('/js/prettify/prettify.js',function(){
         prettyPrint();
     });
-
-    if(/\#comment/.test(location.hash)){
-        $('#disqus_container .comment').trigger('click');
-    }
 })
